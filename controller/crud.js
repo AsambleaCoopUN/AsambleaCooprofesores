df6663125dbacb13fd7103e47a433dd317367121
khronos <jeanvesu@gmail.com>
2023-03-15 14:08:57 -0500

    corrección de consulta query de busqueda de delegado

--- controller/crud.js
+++ controller/crud.js
@@ -2,21 +2,26 @@ const conexion = require('../database/conexion');
 
 exports.save = (req,res)=>{
     const asamblea = req.body.asamblea;
-    const cedula = req.body.cedula;
+    const idUsuario = req.body.idUsuario;
     /* prueba de captura los datos */
 /*     console.log(cedula + " - " + credencia); */
+conexion.query ('INSERT INTO emodel.asistencia_asamblea (asamblea_id, delegado_id SET ?', {asamblea:asamblea, idUsuario:idUsuario}, (error, results) =>{
+    if (error){
+        throw error;
+    }else{
+        res.redirect('/');
+    }
+});
 }
 
 exports.read = (req,res)=>{
     /* prueba de captura los datos */
-    let cedula = (req.body.cedula);
-
-    console.log(cedula);
-    /* conexion.query('select d.delegado_id ,d.delegado_documento_identificacion , d.delegado_nombres , d.delegado_tipo from	emodel.delegado d where  d.delegado_documento_identificacion = ${(cedula)}', (error,results)=>{
+    const cedula = (req.body.cedula);
+    conexion.query(`SELECT a.asamblea_id, d.delegado_id ,d.delegado_documento_identificacion , d.delegado_nombres , d.delegado_tipo FROM emodel.asamblea a, emodel.delegado d WHERE d.delegado_documento_identificacion = '${cedula}'`, (error, results) =>{
         if (error){
             throw error;
         }else{
             res.render('consulta', {results:results.rows});
         }
-    }); */
+    });
 }
