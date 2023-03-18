d28e0f753b18331abfef40083d81151cd2818478
khronos <jeanvesu@gmail.com>
2023-03-18 00:14:09 -0500

    se agrega dropdown a la pagina index, footer a las paginas, reorganización de hojas de estilos se incluye script para el popper, se comenta y organiza el archivo crud y se agregan atributos al archivo gitattributes

--- controller/crud.js
+++ controller/crud.js
@@ -4,11 +4,12 @@ exports.save = (req,res)=>{
     /* capturas las celdas requeridas por el id */
     const asambleaId = (req.body.asambleaId);
     const delegadoId = (req.body.delegadoId);
-
+    const insert = `INSERT INTO emodel.asistencia_asamblea (asamblea_id, delegado_id) VALUES ('${asambleaId}','${delegadoId}')`;
     /* prueba de captura los datos */
     /* console.log(asambleaId + " - " + delegadoId); */
 
-    conexion.query (`INSERT INTO emodel.asistencia_asamblea (asamblea_id, delegado_id) VALUES ('${asambleaId}','${delegadoId}')`, (error, results) => {
+    /* Query de inserción a la base de datos de los campos "asamblea_id" y "delegado_id"*/
+    conexion.query (insert, (error, results) => {
         if (error){
             throw error;
         }else{
@@ -21,6 +22,8 @@ exports.read = (req,res)=>{
     /* prueba de captura los datos */
     const cedula = (req.body.cedula);
     const search = `SELECT a.asamblea_id, d.delegado_id ,d.delegado_documento_identificacion , d.delegado_nombres , d.delegado_tipo FROM emodel.asamblea a, emodel.delegado d WHERE d.delegado_documento_identificacion = '${cedula}'`;
+    
+    /* Query de búsqueda a la base de datos por el número de cedula*/
     conexion.query(search, (error, results) =>{
         if (error){
             throw error;
