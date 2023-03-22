c62ddbad59bd026057cd66b16e4199504481fc31
khronos <jeanvesu@gmail.com>
2023-03-22 17:44:30 -0500

    agrega conexión a base de datos local

--- database/conexion.js
+++ database/conexion.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 /* conexión servidor remoto */
-const pool = new Pool({
+/* const pool = new Pool({
   user: 'adm_evoto',
   password: '4dm_3v0t0',
   host: '20.55.38.188',
@@ -14,25 +14,22 @@ pool.connect((err, client, release) => {
     return console.error('Error adquiriendo el cliente', err.stack)
   }
   console.log('Conexión exitosa a la base de datos')
-});
-
-module.exports = pool;
+}); */
 
 /* conexión servidor local*/
-/* const mysql = require('mysql');
-const conexion = mysql.createConnection({
-  user: 'khronos',
-  password: '123456',
-  host: 'localhost',
-  port: 3306,
-  database: 'asamblea'
+const pool = new Pool({
+  user: 'adm_evoto',
+  password: '4dm_3v0t0',
+  host: '192.168.0.95',
+  port: 5444,
+  database: 'evoto'
 });
 
-conexion.connect((err, client, release)=>{
+pool.connect((err, client, release) => {
   if (err) {
     return console.error('Error adquiriendo el cliente', err.stack)
   }
   console.log('Conexión exitosa a la base de datos')
-})
+});
 
-module.exports = mysql; */
+module.exports = pool;
