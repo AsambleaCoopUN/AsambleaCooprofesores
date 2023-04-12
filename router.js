e21afa74348eb5ad824c9f107a549d08afd85409
khronos <jeanvesu@gmail.com>
2023-04-12 17:04:36 -0500

    se agregan comentarios adicionales

--- router.js
+++ router.js
@@ -2,15 +2,13 @@ const express = require ('express');
 const router = express.Router();
 const conexion = require('./database/conexion');
 
+/* enrutamiento al la página principal  */
 router.get('/',(req,res)=> {
     res.render('index');
-       /*  conexion.query('SELECT * FROM emodel.delegado', (error,results)=>{
-        if (error){
-            throw error;
-        }else{
-            res.send(results);
-        }
-    }); */
+})
+
+router.get('/edit',(req,res)=> {
+    res.render('edit');
 })
 
 /* redirecciona a la pagina donde aparece el listado completo de delegados y lo ordena por fecha y hora de registro de ingreso a la Asamblea*/
@@ -29,10 +27,12 @@ router.get('/view_cociente', (req,res)=>{
     res.render('view_cociente');
 })
 
+/* enrutamiento para visualizar si el usuario existe como delegado o suplente  */
 router.get('/consulta', (req,res)=>{
     res.render('consulta');
 })
 
+/* enrutamiento para visualizar el control de ingreso y salida de sala  */
 router.get('/checkInOut', (req,res)=>{
     res.render('checkInOut');
 })
@@ -43,7 +43,7 @@ router.post('/read', crud.read);
 router.post('/pregunta', crud.pregunta);
 router.post('/salaInOut', crud.salaInOut);
 
-//enrutamiento para visualizar todas las preguntas 
+/* enrutamiento para visualizar todas las preguntas  */
 router.get('/view_questions', (req, res) => {
     const lViewAll = `select pregunta_id, orden_pregunta, pregunta_enunciado, tipo_pregunta, case bandera_votacion when 'E' then 'En espera de votación' when 'C' then 'Pregunta Votada' when 'A' then 'Pregunta en proceso de votación' end estado_pregunta from emodel.pregunta_asamblea pa order by orden_pregunta`;
 
@@ -57,7 +57,7 @@ router.get('/view_questions', (req, res) => {
 });
 
 
-//enrutamiento para visualizar los delegados presentes en la asamblea para validar el Quorum
+/* enrutamiento para visualizar los delegados presentes en la asamblea para validar el Quorum */
 router.get('/estadoEnSala', (req, res) => {
     const courum = `select d.delegado_codigo_alterno codigo_asamblea ,d.delegado_documento_identificacion , d.delegado_nombres , d.delegado_tipo ,
     case aa.asistente_activo 
