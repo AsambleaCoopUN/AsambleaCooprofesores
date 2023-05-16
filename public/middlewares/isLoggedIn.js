b5b4183d0b2dc92f9ce0daca528c14289b03a485
khronos <jeanvesu@gmail.com>
2023-05-16 14:48:29 -0500

    modificación minima de estilo

--- public/middlewares/isLoggedIn.js
+++ public/middlewares/isLoggedIn.js
@@ -1,12 +1,11 @@
 module.exports = (req, res, next) => {
   if (req.cookies.calterno) {
-    const cookieValue = req.cookies.calterno;
+    /* const cookieValue = req.cookies.calterno;
     const cookieData = JSON.parse(cookieValue);
     const asambleaId = cookieData.asambleaId;
     const nombre = cookieData.nombre;
     const alterno = cookieData.alterno;
-    const ipAddress = cookieData.ipAddress;
-    console.log(`validación de cookie:`,asambleaId,nombre,alterno, ipAddress);
+    const ipAddress = cookieData.ipAddress; */
     next();
   } else {
     res.redirect('/login');
