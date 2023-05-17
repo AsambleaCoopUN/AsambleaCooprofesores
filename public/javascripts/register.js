bee74acf916475089e9a1dece7f03f52222073d4
khronos <jeanvesu@gmail.com>
2023-05-17 14:01:58 -0500

    se ajustan radio button y renderizado de la tabla en pregunta.ejs

--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -1,23 +1,26 @@
-document.addEventListener("DOMContentLoaded", function() {
-  function register(){
-    let login = document.getElementById('btnregistrar');
-    console.log(login);
-    login.addEventListener('click', regcookie);
+function register(){
+    let invoto = document.getElementById('regvoto');
+    /* console.log(invoto); */
+    invoto.addEventListener('click', capvoto);
   }
 
-  function regcookie(){
-    const asamblea = document.getElementById("asambleaId").value;
-    const delegado = document.getElementById("delegadoId").value;
-    const alterno = document.getElementById("calterno").value;
+  function capvoto(){
+    const pregunta_id = document.getElementById("pregunta_id").value;
+    const pregEnunciado = document.getElementById("pregEnunciado").value;
+    const cookieValue = req.cookies.calterno; // Obtener el valor de la cookie
+    const cookieData = JSON.parse(cookieValue); // Analizar el valor de la cookie como un objeto JSON
+    const alterno = cookieData.alterno;
 
-    if (asamblea!=""||delegado!=""||alterno!=""){
+    console.log(pregunta_id,pregEnunciado,alterno);
+    
+
+    /* if (asamblea!=""||delegado!=""||alterno!=""){
       const ipAddress = req.header('x-forwarded-for') || req.socket.remoteAddress;
       document.cookie = `calterno=${asamblea};${delegado};${alterno};${ipAddress}`;
       document.location.href="/";
     }else{
       alert("no se encuentran datos para registrar");
-    }
+    } */
   }
 
-  window.addEventListener("load",register);
-});
+window.addEventListener("load",register);
